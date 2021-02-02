33b6b9ce67d4a6960f17729802c15bfe353d2d16
lokeshharad <lokeshharad@gmail.com>
2021-02-02 23:24:03 +0530

    1. Getting Started Wtih Flask Framework      Added home and about pages using app route

--- app.py
+++ app.py
@@ -2,12 +2,15 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-print(app)
-
 
 @app.route('/')
 def home():
-    return "<h1>Hello World!</h1>"
+    return "<h1>Home Page!</h1>"
+
+
+@app.route('/about')
+def about():
+    return "<h1>About Page!</h1>"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
