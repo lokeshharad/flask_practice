ae5ca750d67934fc7add1537d94e9f7cfdb061b2
lokeshharad <lokeshharad@gmail.com>
2021-02-02 23:39:04 +0530

    2. Started with templates      a. home and about html pages added and render those pages (static hmtl pages)

--- app.py
+++ app.py
@@ -5,12 +5,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "<h1>Home Page!</h1>"
+    return render_template('home.html')
 
 
 @app.route('/about')
 def about():
-    return "<h1>About Page!</h1>"
+    return render_template('about.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
