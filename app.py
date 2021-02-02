221389faaa703354bd63485fc90633eb652f29fb
lokeshharad <lokeshharad@gmail.com>
2021-02-03 00:19:15 +0530

    2. Getting stated with templates 	a. created sample database as posts (list of dictonaries about posts) 	b. display those posts on home page using jinja 2 syntax ( for loop ) 	c. added title to page if title parameter is passed to particular app route

--- app.py
+++ app.py
@@ -3,14 +3,30 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+posts = [
+    {
+        'author': 'Lokesh Harad',
+        'title': 'Blog-Post-1',
+        'content': 'First Post Content',
+        'date_posted': 'Feb. 02, 2021'
+    }, 
+    {
+        'author': 'User 2',
+        'title': 'Blog-Post-2',
+        'content': 'Second Post Content',
+        'date_posted': 'Jan. 02, 2021'
+    }
+]
+
+
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 
 @app.route('/about')
 def about():
-    return render_template('about.html')
+    return render_template('about.html', title='About')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
